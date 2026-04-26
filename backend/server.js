44d91bb7e68b081dd3a4fe294434f9320e5a851d
Yash Pratap Singh Solanki <yashpratap0143@gmail.com>
2026-04-26 10:40:16 +0530

    docker file

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve("dist/index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
