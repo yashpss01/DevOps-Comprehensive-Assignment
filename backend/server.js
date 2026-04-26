9a42aba53e6e23c28ede927c2178cd07a9c664cc
Yash Pratap Singh Solanki <yashpratap0143@gmail.com>
2026-04-26 10:07:37 +0530

    file changes

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,12 @@ app.use(express.json());
 
 app.use("/api/users", userRoutes);
 
+app.use(express.static("dist"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve("dist/index.html"));
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create axios instance
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "/api",
 });
 
 // Get all users
